9656ad3e12c55fc214ccce3a1396721906062388
Jason <rametta@outlook.com>
2021-04-08 08:29:33 -0400

    mobile wrapping

--- Web/View/Teams/Show.hs
+++ Web/View/Teams/Show.hs
@@ -9,11 +9,15 @@ instance View ShowView where
             {renderNavbar Nothing mempty mempty}
             <main class="container mx-auto h-full flex flex-col mt-5">
                 <div class="mx-3 flex flex-col">
-                    <div class="flex justify-between">
-                        <h1 class="text-2xl text-white font-bold mb-3">{get #title team} Retros</h1>
-                        <div>
-                            <a href={EditTeamAction $ get #id team} class="mr-2 bg-gray-500 hover:bg-gray-600 text-white font-bold py-1 px-2 rounded transition duration-300">Edit Team</a>
-                            <a href={NewTeamRetroAction $ get #id team} class="bg-green-400 hover:bg-green-300 text-white font-bold py-1 px-2 rounded transition duration-300">New Retro</a>
+                    <div class="flex flex-wrap justify-between mb-3">
+                        <h1 class="text-2xl text-white font-bold">{get #title team} Retros</h1>
+                        <div class="flex">
+                            <div>
+                                <a href={EditTeamAction $ get #id team} class="mr-2 block bg-gray-500 hover:bg-gray-600 text-white font-bold py-1 px-2 rounded transition duration-300">Edit Team</a>
+                            </div>
+                            <div>
+                                <a href={NewTeamRetroAction $ get #id team} class="block bg-green-400 hover:bg-green-300 text-white font-bold py-1 px-2 rounded transition duration-300">New Retro</a>
+                            </div>
                         </div>
                     </div>
                     {renderRetrosOrEmpty}
